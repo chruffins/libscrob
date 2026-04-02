239cdbbfb732f2bb0a5e8fa861e7b69ad4f2be2c
chris lee <chrislee7202@gmail.com>
2026-04-02 14:53:19 -0400

    include guard

--- include/auth.h
+++ include/auth.h
@@ -1,6 +1,10 @@
 #ifndef SCROB_AUTH_H
 #define SCROB_AUTH_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef struct scrob_client scrob_client;
 
 // returns 0 on success, non-zero on failure
@@ -11,5 +15,9 @@ int scrob_get_session_key(scrob_client* client);
 
 // must be freed by caller
 const char* scrob_get_auth_url(scrob_client* client);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
